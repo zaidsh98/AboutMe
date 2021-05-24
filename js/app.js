9938848200c25b2aaf387b0c4105f35b8dbb4440
zaidsh98 <sha7rouri29@gmail.com>
2021-05-24 14:13:41 +0300

    myAge q

--- js/app.js
+++ js/app.js
@@ -4,3 +4,17 @@ let userName = prompt ('Whats your Name');
 console.log(userName);
 alert('Welcome ' + userName);
 
+let permission = prompt ('I will be asking you some yes/no questions would you like to star ?');
+console.log(permission);
+permission = permission.toLowerCase();
+
+if (permission === 'yes' || permission === 'y'){
+    alert('Lets Star');
+}
+else if (permission === 'no'){
+    alert('Sorry for disturbing');
+}
+else {
+    alert('You can only answer with yes or no ');
+}
+
