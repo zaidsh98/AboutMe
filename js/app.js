41840050d1d0d334e38f71e68519d9e2160fe020
zaidsh98 <sha7rouri29@gmail.com>
2021-05-24 14:14:21 +0300

    myAge q

--- js/app.js
+++ js/app.js
@@ -11,8 +11,22 @@ permission = permission.toLowerCase();
 if (permission === 'yes' || permission === 'y'){
     alert('Lets Star');
 }
-else if (permission === 'no'){
+else if (permission === 'no' || permission === 'n'){
     alert('Sorry for disturbing');
+}
+else {
+    alert('You can only answer with yes or no ');
+}
+
+let myAge = prompt ('Am I 25 Years Old ?');
+console.log(myAge);
+myAge = myAge.toLowerCase();
+
+if (myAge === 'yes' || myAge === 'y'){
+    alert('Thats Wrong Answer');
+}
+else if (myAge === 'no' || myAge === 'n'){
+    alert('Thats Correct');
 }
 else {
     alert('You can only answer with yes or no ');
